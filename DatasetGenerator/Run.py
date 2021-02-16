ada3b4e301c30b4591b5c5b85619d8baa587f858
ferdinandl007 <ferdinandloesch@me.com>
2021-02-16 10:26:46 +0100

    .

--- DatasetGenerator/Run.py
+++ DatasetGenerator/Run.py
@@ -2,9 +2,5 @@ from GenerateDataset import GenerateDataset
 from CleanDataSet import CleanDataSet
 import os
 
-# dataset_generator = GenerateDataset(every_x_frame=13, set_fix_image_size=384)
-# dataset_generator.run()
-
-
-path = os.path.dirname(__file__) + "/faces"
-CleanDataSet().resize(path, 384)
+dataset_generator = GenerateDataset(every_x_frame=13, set_fix_image_size=384)
+dataset_generator.run()
